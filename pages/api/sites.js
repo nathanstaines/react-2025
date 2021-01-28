b3e26dfc469e44911a665f2960899fe9d861e09f
Nathan Staines <nathan@nathanstaines.com>
2021-01-28 15:13:24 +0930

    Add logging and handle exceptions in API routes (#9)

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,3 +1,5 @@
+import { formatHeaders, logger } from '@/utils/logger';
+
 import { auth } from '@/lib/firebase-admin';
 import { getUserSites } from '@/lib/db-admin';
 
@@ -8,6 +10,20 @@ export default async (req, res) => {
 
     res.status(200).json({ sites });
   } catch (error) {
+    logger.error(
+      {
+        request: {
+          headers: formatHeaders(req.headers),
+          url: req.url,
+          method: req.method,
+        },
+        response: {
+          statusCode: res.statusCode,
+        },
+      },
+      error.message
+    );
+
     res.status(500).json({ error });
   }
 };
